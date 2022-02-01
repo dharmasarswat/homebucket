d9938f083b063e2f8818f88efdc0c883c99499e8
dharmasarswat <dharmasarswat001@gmail.com>
2022-02-01 09:25:27 +0530

    chenges

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" exact element={<Home />} />
+        <Route path="/homebucket" exact element={<Home />} />
         <Route path="/login" exact element={<Login />} />
         <Route path="/register" exact element={<Register />} />
         <Route path="/user" element={<Dashboard />} />
