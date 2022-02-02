191d1c7946cede29b687247b4a8483744f299f6e
dharmasarswat <dharmasarswat001@gmail.com>
2022-02-02 08:16:45 +0530

    added backend endpoint

--- src/ApolloClient/client.js
+++ src/ApolloClient/client.js
@@ -2,7 +2,7 @@ import { ApolloClient, ApolloLink, InMemoryCache, HttpLink } from "@apollo/clien
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = new HttpLink({
-  uri: "http://localhost:4000/graphql",
+  uri: "https://homebucketbe.herokuapp.com/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
